ac5944766ed9f542ad447f00b76055138002bec0
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-10-25 16:14:07 +0200

    add BlogPost_Collection schema

---
+++ apps/sanity/schema/collectionTypes/BlogPost_Collection.ts
@@ -0,0 +1,46 @@
+import { defineField, defineType } from "sanity";
+import { defineSlugForDocument } from "../../utils/define-slug-for-document";
+
+const name = 'BlogPost_Collection';
+const title = 'Blog Post Collection';
+const icon = () => '📰';
+
+export default defineType({
+  name: name,
+  type: 'document',
+  title,
+  icon,
+  options: { documentPreview: true },
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+      validation: Rule => Rule.required(),
+    }),
+    ...defineSlugForDocument({ source: 'name', prefix: '/pl/blog/' }),
+    defineField({
+      name: 'seo',
+      type: 'seo',
+      title: 'SEO',
+      group: 'seo',
+    }),
+  ],
+  groups: [
+    {
+      name: 'seo',
+      title: 'SEO',
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      slug: 'slug.current',
+    },
+    prepare: ({ name, slug }) => ({
+      title: name,
+      subtitle: slug,
+      icon,
+    }),
+  },
+});
