a5fde44f75ef24070bfa05e0bdc50b5a3ff2107a
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-10-25 16:14:17 +0200

    add CaseStudy_Collection schema

---
+++ apps/sanity/schema/collectionTypes/CaseStudy_Collection.ts
@@ -0,0 +1,46 @@
+import { defineField, defineType } from "sanity";
+import { defineSlugForDocument } from "../../utils/define-slug-for-document";
+
+const name = 'CaseStudy_Collection';
+const title = 'Case Study Collection';
+const icon = () => '📂';
+
+export default defineType({
+  name: name,
+  type: 'document',
+  title,
+  icon,
+  options: { documentPreview: true },
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+      validation: Rule => Rule.required(),
+    }),
+    ...defineSlugForDocument({ source: 'name', prefix: '/pl/portfolio/' }),
+    defineField({
+      name: 'seo',
+      type: 'seo',
+      title: 'SEO',
+      group: 'seo',
+    }),
+  ],
+  groups: [
+    {
+      name: 'seo',
+      title: 'SEO',
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      slug: 'slug.current',
+    },
+    prepare: ({ name, slug }) => ({
+      title: name,
+      subtitle: slug,
+      icon,
+    }),
+  },
+});
