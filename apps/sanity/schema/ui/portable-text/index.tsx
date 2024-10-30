c59753afa6472e453c57d129a67fa85df6d93646
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-10-30 11:37:48 +0100

    add types with defineField to portable-text schema

--- apps/sanity/schema/ui/portable-text/index.tsx
+++ apps/sanity/schema/ui/portable-text/index.tsx
@@ -11,87 +11,90 @@ export const PortableText = ({ name, title, allowHeadings = false }: { name?: st
     // @ts-ignore
     input: CustomInput
   },
-  of: [{
-    type: 'block',
-    styles: [
-      { title: 'Normal', value: 'normal' },
-      ...(allowHeadings ? [
-        { title: 'Heading 2', value: 'h2' },
-        { title: 'Heading 3', value: 'h3' }
-      ] : [])
-    ],
-    lists: [
-      { title: 'Bullet', value: 'bullet' },
-      { title: 'Numbered', value: 'number' }
-    ],
-    marks: {
-      decorators: [
-        { title: 'Strong', value: 'strong' },
-        { title: 'Emphasis', value: 'em' }
+  of: [
+    defineField({
+      type: 'block',
+      name: 'block',
+      styles: [
+        { title: 'Normal', value: 'normal' },
+        ...(allowHeadings ? [
+          { title: 'Heading 2', value: 'h2' },
+          { title: 'Heading 3', value: 'h3' }
+        ] : [])
       ],
-      annotations: [
-        defineField({
-          name: 'link',
-          type: 'object',
-          title: 'Link',
-          icon: () => '🔗',
-          fields: [
-            defineField({
-              name: 'linkType',
-              type: 'string',
-              title: 'Type',
-              description: 'Choose "External" for links to websites outside your domain, or "Internal" for links to pages within your site.',
-              options: {
-                list: ['external', 'internal'],
-                layout: 'radio',
-                direction: 'horizontal',
-              },
-              initialValue: 'external',
-            }),
-            defineField({
-              name: 'external',
-              type: 'string',
-              title: 'URL',
-              description: 'Specify the full URL. Ensure it starts with "https://", "mailto:" or "tel:" protocol.',
-              hidden: ({ parent }) => parent?.linkType !== 'external',
-              validation: (Rule) => [
-                Rule.custom((value, { parent }) => {
-                  const linkType = (parent as { linkType?: string })?.linkType;
-                  if (linkType === 'external') {
-                    if (!value) return "URL is required";
-                    if (!value.startsWith('https://') && !value.startsWith('mailto:') && !value.startsWith('tel:')) {
-                      return 'External link must start with the "https://", "mailto:" or "tel:" protocol';
+      lists: [
+        { title: 'Bullet', value: 'bullet' },
+        { title: 'Numbered', value: 'number' }
+      ],
+      marks: {
+        decorators: [
+          { title: 'Strong', value: 'strong' },
+          { title: 'Emphasis', value: 'em' }
+        ],
+        annotations: [
+          defineField({
+            name: 'link',
+            type: 'object',
+            title: 'Link',
+            icon: () => '🔗',
+            fields: [
+              defineField({
+                name: 'linkType',
+                type: 'string',
+                title: 'Type',
+                description: 'Choose "External" for links to websites outside your domain, or "Internal" for links to pages within your site.',
+                options: {
+                  list: ['external', 'internal'],
+                  layout: 'radio',
+                  direction: 'horizontal',
+                },
+                initialValue: 'external',
+              }),
+              defineField({
+                name: 'external',
+                type: 'string',
+                title: 'URL',
+                description: 'Specify the full URL. Ensure it starts with "https://", "mailto:" or "tel:" protocol.',
+                hidden: ({ parent }) => parent?.linkType !== 'external',
+                validation: (Rule) => [
+                  Rule.custom((value, { parent }) => {
+                    const linkType = (parent as { linkType?: string })?.linkType;
+                    if (linkType === 'external') {
+                      if (!value) return "URL is required";
+                      if (!value.startsWith('https://') && !value.startsWith('mailto:') && !value.startsWith('tel:')) {
+                        return 'External link must start with the "https://", "mailto:" or "tel:" protocol';
+                      }
+                      if (!isValidUrl(value)) return 'Invalid URL';
                     }
-                    if (!isValidUrl(value)) return 'Invalid URL';
-                  }
-                  return true;
-                }),
-              ],
-            }),
-            defineField({
-              name: 'internal',
-              type: 'reference',
-              title: 'Internal reference to page',
-              description: 'Select an internal page to link to.',
-              to: InternalLinkableTypes,
-              options: {
-                disableNew: true,
-                filter: 'defined(slug.current)',
-              },
-              hidden: ({ parent }) => parent?.linkType !== 'internal',
-              validation: (rule) => [
-                rule.custom((value, { parent }) => {
-                  const linkType = (parent as { linkType?: string })?.linkType;
-                  if (linkType === 'internal' && !value?._ref) return "You have to choose internal page to link to.";
-                  return true;
-                }),
-              ],
-            }),
-          ]
-        }),
-      ]
-    }
-  }],
+                    return true;
+                  }),
+                ],
+              }),
+              defineField({
+                name: 'internal',
+                type: 'reference',
+                title: 'Internal reference to page',
+                description: 'Select an internal page to link to.',
+                to: InternalLinkableTypes,
+                options: {
+                  disableNew: true,
+                  filter: 'defined(slug.current)',
+                },
+                hidden: ({ parent }) => parent?.linkType !== 'internal',
+                validation: (rule) => [
+                  rule.custom((value, { parent }) => {
+                    const linkType = (parent as { linkType?: string })?.linkType;
+                    if (linkType === 'internal' && !value?._ref) return "You have to choose internal page to link to.";
+                    return true;
+                  }),
+                ],
+              }),
+            ]
+          }),
+        ]
+      }
+    }),
+  ]
 });
 
 export default PortableText({});
