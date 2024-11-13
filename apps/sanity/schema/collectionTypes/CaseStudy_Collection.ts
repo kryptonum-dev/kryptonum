6042508efc90f00026ac7c3c4ad2679e68a34329
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-11-13 15:54:31 +0100

    add detailed info for DetailedInfoAndStats section

--- apps/sanity/schema/collectionTypes/CaseStudy_Collection.ts
+++ apps/sanity/schema/collectionTypes/CaseStudy_Collection.ts
@@ -1,5 +1,6 @@
 import { defineField, defineType } from "sanity";
 import { defineSlugForDocument } from "../../utils/define-slug-for-document";
+import { TextInput } from "sanity";
 
 const name = 'CaseStudy_Collection';
 const title = 'Case Study Collection';
@@ -19,6 +20,52 @@ export default defineType({
       validation: Rule => Rule.required(),
     }),
     ...defineSlugForDocument({ source: 'name', prefix: '/pl/portfolio/' }),
+    defineField({
+      name: 'sector',
+      type: 'string',
+      title: 'Sector (optional)',
+    }),
+    defineField({
+      name: 'client',
+      type: 'string',
+      title: 'Client (optional)',
+    }),
+    defineField({
+      name: 'categories',
+      type: 'array',
+      title: 'Categories',
+      of: [
+        defineField({
+          name: 'category',
+          type: 'reference',
+          to: [{ type: 'CaseStudyCategory_Collection' }],
+          options: {
+            disableNew: true,
+            filter: ({ parent }) => {
+              const selectedIds = (parent as { _ref?: string }[])?.filter(item => item._ref).map(item => item._ref) || [];
+              if (selectedIds.length > 0) {
+                return {
+                  filter: '!(_id in $selectedIds) && !(_id in path("drafts.**"))',
+                  params: { selectedIds }
+                }
+              }
+              return {}
+            }
+          }
+        }),
+      ],
+      validation: Rule => Rule.required(),
+    }),
+    defineField({
+      name: 'date',
+      type: 'date',
+      title: 'Date',
+      description: 'Only year will be shown',
+      options: {
+        dateFormat: "YYYY",
+      },
+      validation: Rule => Rule.required(),
+    }),
     defineField({
       name: 'components',
       type: 'components',
