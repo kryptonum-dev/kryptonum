e52448905d9e54caa92f32d7cdc133941adaef76
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-12-04 14:21:28 +0100

    hide query function from revalidate api route

--- apps/astro/src/pages/api/revalidate.ts
+++ apps/astro/src/pages/api/revalidate.ts
@@ -1,6 +1,5 @@
 export const prerender = false;
 
-import sanityFetch from "@/utils/sanity.fetch";
 
 const SANITY_REVALIDATE_BEARER = import.meta.env.SANITY_REVALIDATE_BEARER || process.env.SANITY_REVALIDATE_BEARER;
 
@@ -19,22 +18,21 @@ export async function POST({ request }: { request: Request }) {
   const { tag, id } = (await request.json()) as RequestType;
 
 
-  const query = async (tag: string, id?: string) => {
-    let queryHeader = `*[_type == "${tag}"][0]`;
-    if (id) queryHeader = `*[_type == "${tag}" && _id == "${id}"][0]`;
-    return await sanityFetch<{ references: string[] }>({
-      query: /* groq */ `
-        ${queryHeader}{
-          "references": *[references(^._id)]._type,
-        }
-      `,
-    });
-  };
+  // const query = async (tag: string, id?: string) => {
+  //   let queryHeader = `*[_type == "${tag}"][0]`;
+  //   if (id) queryHeader = `*[_type == "${tag}" && _id == "${id}"][0]`;
+  //   return await sanityFetch<{ references: string[] }>({
+  //     query: /* groq */ `
+  //       ${queryHeader}{
+  //         "references": *[references(^._id)]._type,
+  //       }
+  //     `,
+  //   });
+  // };
 
   console.log(request.headers.get("Authorization"));
   console.log(process.env.VERCEL_DEPLOYMENT_ID);
   console.log(await request.json());
-  console.log(await query(tag, id));
 
   return new Response(JSON.stringify({ revalidated: false, timestamp: Date.now() }), { status: 200 });
 }
